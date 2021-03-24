16f975f4cee748fa4985f7da867591bd6668caab
superbeom <it.super.beom@gmail.com>
2021-03-25 07:06:21 +0900

    #11.4 Photo Component part One

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,56 @@
-import { logUserOut } from "../apollo";
+import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+import Avatar from "../components/Avatar";
+import { FatText } from "../components/shared";
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin-bottom: 20px;
+`;
+
+const PhotoHeader = styled.div`
+  display: flex;
+  padding: 5px 10px;
+  align-items: center;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 5px;
+`;
 
 export default () => {
+  const { data } = useQuery(FEED_QUERY);
+  console.log(data);
+
   return (
     <div>
-      <h1>Home</h1>
-      <button onClick={() => logUserOut()}>Logout</button>
+      {data?.seeFeed?.map((photo) => (
+        <PhotoContainer key={photo.id}>
+          <PhotoHeader>
+            <Avatar url={photo.user.avatar} />
+            <Username>{photo.user.username}</Username>
+          </PhotoHeader>
+        </PhotoContainer>
+      ))}
     </div>
   );
 };
