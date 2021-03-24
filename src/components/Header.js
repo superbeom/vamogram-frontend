d1e4e80a86831433c9f76dc888b89e931bdc0c81
superbeom <it.super.beom@gmail.com>
2021-03-24 17:43:19 +0900

    #11.1 Header part Two

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faCompass, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "../apollo";
+import routes from "../routes";
+import useUser from "../hooks/useUser";
 
 const StyledHeader = styled.header`
   width: 100%;
@@ -27,23 +32,46 @@ const Icon = styled.span`
   margin-left: 15px;
 `;
 
-export default () => (
-  <StyledHeader>
-    <Wrapper>
-      <Column>
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-      </Column>
-      <Column>
-        <Icon>
-          <FontAwesomeIcon icon={faHome} size="lg" />
-        </Icon>
-        <Icon>
-          <FontAwesomeIcon icon={faCompass} size="lg" />
-        </Icon>
-        <Icon>
-          <FontAwesomeIcon icon={faUser} size="lg" />
-        </Icon>
-      </Column>
-    </Wrapper>
-  </StyledHeader>
-);
+const Button = styled.span`
+  background-color: ${(props) => props.theme.accent};
+  border-radius: 4px;
+  padding: 4px 15px;
+  color: white;
+  font-weight: 600;
+`;
+
+export default () => {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const loggedInUser = useUser();
+
+  console.log("loggedInUser: ", loggedInUser);
+
+  return (
+    <StyledHeader>
+      <Wrapper>
+        <Column>
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </Column>
+        <Column>
+          {isLoggedIn ? (
+            <>
+              <Icon>
+                <FontAwesomeIcon icon={faHome} size="lg" />
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faCompass} size="lg" />
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faUser} size="lg" />
+              </Icon>
+            </>
+          ) : (
+            <Link to={routes.home}>
+              <Button>Login</Button>
+            </Link>
+          )}
+        </Column>
+      </Wrapper>
+    </StyledHeader>
+  );
+};
