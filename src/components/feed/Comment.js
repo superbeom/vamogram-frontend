ed0d0c121a9f6d05876c69d7c3cb940a7d2ebc30
superbeom <it.super.beom@gmail.com>
2021-03-25 17:36:55 +0900

    #11.13 Parsing Hashtags

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,17 +1,39 @@
 import styled from "styled-components";
+import sanitizeHtml from "sanitize-html";
 import { FatText } from "../shared";
 
 const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
-const Comment = ({ author, payload }) => (
-  <CommentContainer>
-    <FatText>{author}</FatText>
-    <CommentCaption>{payload}</CommentCaption>
-  </CommentContainer>
-);
+const Comment = ({ author, payload }) => {
+  const cleanedPayload = sanitizeHtml(
+    payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
+    {
+      allowedTags: ["mark"],
+    }
+  );
+
+  return (
+    <CommentContainer>
+      <FatText>{author}</FatText>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
+    </CommentContainer>
+  );
+};
 
 export default Comment;
