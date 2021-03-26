e48db9ab054485498ea98963839a31ee30d16f27
superbeom <it.super.beom@gmail.com>
2021-03-26 12:11:50 +0900

    #12.1 Queries and Fragments

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,9 +1,38 @@
 import { useParams } from "react-router";
+import { gql, useQuery } from "@apollo/client";
+import { PHOTO_FRAGMENT } from "../fragments";
+
+const SEE_PROFILE_QUERY = gql`
+  query seeProfile($username: String!) {
+    seeProfile(username: $username) {
+      firstName
+      lastName
+      username
+      bio
+      avatar
+      photos {
+        ...PhotoFragment
+      }
+      totalFollowers
+      totalFollowing
+      isMe
+      isFollowing
+    }
+  }
+  ${PHOTO_FRAGMENT}
+`;
 
 const Profile = () => {
   const { username } = useParams();
+  const { data, loading } = useQuery(SEE_PROFILE_QUERY, {
+    variables: {
+      username,
+    },
+  });
 
-  console.log("username: ", username);
+  if (!loading) {
+    console.log(data);
+  }
 
   return "Profile";
 };
